171442074a773e4dd42b2d403f60c5106d8df4e6
administrator <gvw@san.rr.com>
2015-07-29 16:59:59 -0700

    We wade the robot go in inches as far as you wan
    It goes how far you want it in inches

---
+++ RedBotDriveDistance/RedBotDriveDistance.ino
@@ -0,0 +1,41 @@
+/***********************************************************************
+ * Exp2_DriveForward -- RedBot Experiment 2
+ *
+ * Drive forward and stop.
+ *
+ * Hardware setup:
+ * The Power switch must be on, the motors must be connected, and the board must be receiving power
+ * from the battery. The motor switch must also be switched to RUN.
+ *
+ * 23 Sept 2013 N. Seidle/M. Hord
+ * 04 Oct 2014 B. Huang
+ ***********************************************************************/
+
+#include <RedBot.h>  // This line "includes" the RedBot library into your sketch.
+// Provides special objects, methods, and functions for the RedBot.
+
+RedBotMotors motors; // Instantiate the motor control object. This only needs
+// to be done once.
+
+void setup()
+{
+  driveDistance(20);
+}
+
+void loop()
+{
+  // Nothing here. We'll get to this in the next experiment.
+}
+void driveDistance(int distance)
+{
+  int avgSpeed = 13;
+  int motorsPower = 135;
+  long driveTime;
+  driveTime = (long) 1000 * distance / avgSpeed;
+  motors.drive(motorsPower);
+  delay(driveTime);
+  motors.brake();
+}
+
+
+
